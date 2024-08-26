17f1f079835cdf42d54339773864515275b95e80
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-08-26 19:28:08 +0530

    User schema, graphql schema, resolvers modified

--- server/src/graphql/resolvers/resolvers.ts
+++ server/src/graphql/resolvers/resolvers.ts
@@ -1,6 +1,6 @@
 import { FilterQuery } from "mongoose";
 import { IUser, User } from "../../models/User.js";
-import { getAllUsers, getUserById, updateUser } from "../../controllers/userController.js";
+import { addUserDetails, getAllUsers, getUserById, updateUser } from "../../controllers/userController.js";
 import { createProject, getAllProjects, getProjectById } from "../../controllers/projectController.js";
 import { createFlowchart, getAllFlowcharts, getFlowchartById } from "../../controllers/roadmapController.js";
 
@@ -17,6 +17,7 @@ export const graphqlResolvers = {
   Mutation: {
     UPDATE_USER: updateUser,
     CREATE_PROJECT: createProject,
-    CREATE_FLOWCHART: createFlowchart
+    CREATE_FLOWCHART: createFlowchart,
+    ADD_USER_BASIC_DETAILS: addUserDetails,
   },
 };

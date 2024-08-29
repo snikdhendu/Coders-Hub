39e7d3924ba2767dab7282a72f88a4717222e59b
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-08-30 00:54:14 +0530

    getAllProjects Query changed

--- Frontend/src/graphql/query/projectQuery.ts
+++ Frontend/src/graphql/query/projectQuery.ts
@@ -2,16 +2,20 @@ import { gql } from "@apollo/client";
 
 export const GET_ALL_PROJECTS = gql`
   query GetAllProjects {
-    getAllProjects {
-      _id
-      projectName
-      tagline
-      description
-      technologies
-      githubRepoLink
-      liveLink
-      images
-      logo
-    }
+  getAllProjects {
+    _id
+    projectName
+    tagline
+    description
+    githubRepoLink
+    liveLink
+    technologies
+    images
+    logo
+    clerkUserId
+    firstName
+    lastName
+    profileUrl
   }
+}
 `;
